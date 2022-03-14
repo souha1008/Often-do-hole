c432732e469d422ea75b4f170f2bc4be5a876507
高平 大数 <takatai2020game@gmail.com>
2022-03-15 01:53:06 +0900

    ジャンプ台スクリプトと計算用スクリプト追加(仮)

---
+++ MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_SpringBoard.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmick_SpringBoard : MonoBehaviour
+{
+    public float SpringPower = 3000.0f;   // ジャンプ台の力
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            float Rad;           // 回転角
+            Vector3 VecPower;    // 加えるベクトル量
+
+            //PlayerMain playermain = collider.gameObject.GetComponent<PlayerMain>();
+            Rigidbody Rb = collider.gameObject.transform.GetComponent<Rigidbody>(); // リジッドボディ
+            Rad = this.transform.localEulerAngles.z;  // ジャンプ台の回転角
+            Rad = CalculationScript.AngleCalculation(Rad); // 角度ラジアン変換
+            VecPower = CalculationScript.AngleVectorXY(Rad) * SpringPower;
+
+            if (VecPower.x < 1 && VecPower.x > -1) VecPower.x = 0;
+            if (VecPower.y < 1 && VecPower.y > -1) VecPower.y = 0;
+
+            Debug.Log(VecPower.x);
+            Debug.Log(VecPower.y);
+
+            //playermain.vel += new Vector2 (VecPower.x, VecPower.y);
+            Rb.velocity = new Vector3 (VecPower.x, VecPower.y, 0);
+            //Rb.AddForce(VecPower,ForceMode.VelocityChange);
+        }
+    }
+}
