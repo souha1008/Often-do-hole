fa8a2c30168999116c8c6312cff1a2056fa4acf1
高平 大数 <takatai2020game@gmail.com>
2022-03-18 18:33:12 +0900

    ギミック落ちるブロック更新

--- MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_FallBlock.cs
+++ MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_FallBlock.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class Gimmick_FallBlock : Gimmick_Main
 {
-    private bool NowFall = false;    // 落下中か
+    public float FallPower = 0.05f;     // 落ちる力
+    private bool NowFall;               // 落下中か
 
     public override void Init()
     {
+        // 初期化
+        NowFall = false;
 
+        // コリジョン
+        this.gameObject.GetComponent<Collider>().isTrigger = false;  // トリガーオフ
+
+        // リジッドボディ
+        Rb.mass = 100000.0f; // 重くして動かないようにする
     }
 
     public override void Move()
     {
-
+        if (NowFall)
+        {
+            Vel.y += -FallPower;
+        }
+        if (TotalMoveVel.y <= -500.0f)
+        {
+            Death();
+        }
     }
 
     public override void Death()
     {
-
+        // 自分自身を消す
+        Destroy(this.gameObject);
     }
 
     public override void OnTriggerEnter(Collider collider)
     {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
         // プレイヤーか錨と接触
-        if (collider.gameObject.tag == "Player" && collider.gameObject.tag == "Bullet")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Bullet")
         {
-            NowFall = true;
+            NowFall = true; // 落下中
         }
     }
 }
