8e8fbdb994325ce1c8367f31d0f1f00f35943d7a
高平 大数 <takatai2020game@gmail.com>
2022-03-18 14:47:49 +0900

    Direction_UIのPlayerScriptにプレイヤーアタッチ 仮ギミック追加

---
+++ MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_MoveBlock.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmick_MoveBlock : Gimmick_Main
+{
+    public override void Death()
+    {
+
+    }
+
+    public override void Init()
+    {
+
+    }
+
+    public override void Move()
+    {
+
+    }
+
+    public override void OnTriggerEnter(Collider collider)
+    {
+
+    }
+}
