d9ba253d09d442ea8b6531b8a6d0a1058294491a
高平 大数 <takatai2020game@gmail.com>
2022-03-20 16:07:24 +0900

    ギミック落下ブロック処理更新

--- MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_FallBlock.cs
+++ MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_FallBlock.cs
@@ -30,29 +30,29 @@ public class Gimmick_FallBlock : Gimmick_Main
         Rb.isKinematic = true;  // キネマティックオン
     }
 
-    public override void UpdateMove()
+    public override void FixedMove()
     {
         // ポジション変更
         if (NowFall)
         {
-            //Vector3 OldPos = this.gameObject.transform.position;
+            Vector3 OldPos = this.gameObject.transform.position;
             this.gameObject.transform.position = new Vector3(StartPos.x, Easing.QuartIn(NowTime, FallTime, StartPos.y, StartPos.y - FallLength), StartPos.z);
             NowTime += Time.deltaTime;
 
             // プレイヤーからレイ飛ばして真下にブロックがあったら
-            //if (PlayerObject != null)
-            //{
-            //    Ray ray = new Ray(PlayerObject.transform.position, Vector3.down);
-            //    RaycastHit hit;
-            //    if (Physics.Raycast(ray, out hit, 1.5f))
-            //    {
-            //        if(hit.collider.gameObject == this.gameObject)
-            //        {
-            //            PlayerObject.gameObject.transform.position =
-            //                PlayerObject.gameObject.transform.position + new Vector3(0, this.gameObject.transform.position.y - OldPos.y, 0);
-            //        }
-            //    }
-            //}
+            if (PlayerObject != null)
+            {
+                Ray ray = new Ray(PlayerObject.transform.position, Vector3.down);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1.5f))
+                {
+                    if (hit.collider.gameObject == this.gameObject)
+                    {
+                        PlayerObject.gameObject.transform.position =
+                            PlayerObject.gameObject.transform.position + new Vector3(0, this.gameObject.transform.position.y - OldPos.y, 0);
+                    }
+                }
+            }
         }
         if (NowTime > FallTime)
         {
@@ -72,7 +72,6 @@ public class Gimmick_FallBlock : Gimmick_Main
         if (collision.gameObject.tag == "Bullet")
         {
             NowFall = true; // 落下中
-            //Rb.isKinematic = false;
         }
         else if (collision.gameObject.tag == "Player")
         {
@@ -82,7 +81,6 @@ public class Gimmick_FallBlock : Gimmick_Main
             if (Physics.Raycast(ray_1, out hit, 1.5f))
             {
                 NowFall = true; // 落下中
-                //Rb.isKinematic = false;
                 PlayerObject = collision.gameObject;
             }
         }
