f1a0a41736739c0db30386d7782be41b22dded7e
藤代 遥稀 <qqcu8ef9k@peace.ocn.ne.jp>
2022-03-26 11:01:39 +0900

    DOTween追加

--- MorningStarAlpha/Assets/Script/Fujishiro Script/Raen_Path.cs
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/Raen_Path.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using DG.Tweening;
 
 public class Raen_Path : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable] public struct Waypoint
     {
-        
+        [Tooltip("ウェイポイントの位置")]
+        public Vector3 position;
+
+        [Tooltip("ウェイポイントに到達したときの角度")]
+        public float roll;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Waypoint[] m_waypoints = new Waypoint[0];
+
+    // アタッチしたときに最初に設定される
+    void Reset()
     {
-        
+        m_waypoints = new Waypoint[2]
+        {
+            new Waypoint{position = new Vector3(0, 0, 0), roll = 0},
+            new Waypoint{position = new Vector3(0, 0, 0),roll = 0}
+        };
     }
 }
