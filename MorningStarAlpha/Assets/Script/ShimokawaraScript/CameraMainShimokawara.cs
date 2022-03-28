37fa44f2c1ea12ac7e039cf82593b37c5e5ef8be
MSI\clclm <hal88k5@gmail.com>
2022-03-28 20:33:43 +0900

    カメラいい感じになった

--- MorningStarAlpha/Assets/Script/ShimokawaraScript/CameraMainShimokawara.cs
+++ MorningStarAlpha/Assets/Script/ShimokawaraScript/CameraMainShimokawara.cs
@@ -17,11 +17,17 @@ public class CameraMainShimokawara: MonoBehaviour
     int CameraTrueCnt;//カメラが正しい位置を探索するフレームを数える
     int CAMERA_RETURN_FLAME = 250;//カメラが何フレームかけて正しい場所に行くか
 
+    //振り子やめた瞬間を拾うために、勝手に保存してる
+    EnumPlayerState PlayerState;
+    EnumPlayerState OldPlayerState;
+
     private void Start()
     {
         instance = this;
         CameraTrueCnt = CAMERA_RETURN_FLAME;
         TraceObj();
+
+        PlayerState = OldPlayerState = PlayerMain.instance.refState;
     }
 
     public void ManualUpdate()
@@ -42,8 +48,16 @@ public class CameraMainShimokawara: MonoBehaviour
     //プレイヤーをカメラの中央に収め続ける
     void TraceObj()
     {
+        OldPlayerState = PlayerState;
+        PlayerState = PlayerMain.instance.refState;
+
         if(isRail)
         {
+            //振り子じゃなくなったタイミングで、CameraTrueCntを0にする
+            if(OldPlayerState == EnumPlayerState.SWING && PlayerState != EnumPlayerState.SWING)
+            {
+                CameraTrueCnt = 0;
+            }
 
 
             TruePos.x = XObj.transform.position.x;
@@ -56,18 +70,34 @@ public class CameraMainShimokawara: MonoBehaviour
             {
                 this.transform.position = new Vector3(TruePos.x, transform.position.y, transform.position.z);
 
-                CameraTrueCnt = 0;
             }
             //振り子じゃない時
             else
-            {
-                //Fixedにやってもらう
+            {                
+                //↓Fixedにやってもらう
                 //CameraTrueCnt++;
 
                 if(CameraTrueCnt <= CAMERA_RETURN_FLAME)
                 {
-                    //20フレームかけて元の場所に戻る
-                    this.transform.position = Vector3.Lerp(this.transform.position, TruePos,(float)CameraTrueCnt / CAMERA_RETURN_FLAME);
+                    //今のカウントをイージングして、初速と再終息を遅めにする
+                    float EasingCnt;
+                    if (CameraTrueCnt < CAMERA_RETURN_FLAME / 2)
+                    {
+                        EasingCnt = CameraTrueCnt * Easing.EasingTypeFloat(EASING_TYPE.QUAD_IN, CameraTrueCnt, (CAMERA_RETURN_FLAME / 2 - 1), 0, 1);
+                    }
+                    else if(CameraTrueCnt > CAMERA_RETURN_FLAME / 2)
+                    {
+                        EasingCnt = CameraTrueCnt * Easing.EasingTypeFloat(EASING_TYPE.QUAD_IN, CAMERA_RETURN_FLAME - CameraTrueCnt, (CAMERA_RETURN_FLAME / 2 - 1), 0, 1);
+
+                    }
+                    else
+                    {
+                        EasingCnt = CameraTrueCnt;
+                    }
+
+                    //20フレームかけてYを元の場所に戻る
+                    float Y = Mathf.Lerp(this.transform.position.y, TruePos.y, (float)EasingCnt / CAMERA_RETURN_FLAME);
+                    this.transform.position = new Vector3(TruePos.x, Y, TruePos.z);
                 }
                 else
                 {
@@ -91,6 +121,37 @@ public class CameraMainShimokawara: MonoBehaviour
     void FixedUpdate()
     {
         CameraTrueCnt = Mathf.Min(CameraTrueCnt + 1, 500);
+    }
+
+    //あるべき場所に寄せる関数
+    void Bring()
+    {
+        //今のカウントをイージングして、初速と再終息を遅めにする
+        float EasingCnt;
+        //寄せ終わってる
+        if (CameraTrueCnt > CAMERA_RETURN_FLAME)
+        {
+            EasingCnt = CAMERA_RETURN_FLAME;
+        }
+        //寄せ中
+        else
+        {
+            if (CameraTrueCnt < CAMERA_RETURN_FLAME / 2)
+            {
+                EasingCnt = CameraTrueCnt * Easing.EasingTypeFloat(EASING_TYPE.QUAD_IN, CameraTrueCnt, (CAMERA_RETURN_FLAME / 2 - 1), 0, 1);
+            }
+            else if (CameraTrueCnt > CAMERA_RETURN_FLAME / 2)
+            {
+                EasingCnt = CameraTrueCnt * Easing.EasingTypeFloat(EASING_TYPE.QUAD_IN, CAMERA_RETURN_FLAME - CameraTrueCnt, (CAMERA_RETURN_FLAME / 2 - 1), 0, 1);
+            }
+            else
+            {
+                EasingCnt = CameraTrueCnt;
+            }
+        }
+
+        //20フレームかけて元の場所に戻る
+        this.transform.position = Vector3.Lerp(this.transform.position, TruePos, (float)EasingCnt / CAMERA_RETURN_FLAME);
     }
 }
 
