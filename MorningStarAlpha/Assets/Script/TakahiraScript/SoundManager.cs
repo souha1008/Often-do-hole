bf88eefc6d478c6d47e12710717344fa8003ba7b
高平 大数 <takatai2020game@gmail.com>
2022-03-29 17:07:40 +0900

    パッケージマネージャーからAddressable Assets追加

--- MorningStarAlpha/Assets/Script/TakahiraScript/SoundManager.cs
+++ MorningStarAlpha/Assets/Script/TakahiraScript/SoundManager.cs
@@ -1,37 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
+//#if UNITY_EDITOR
+//using UnityEditor;
 
 
-// SoundManagerクラスを拡張
-[CustomEditor(typeof(SoundManager))]
+//// SoundManagerクラスを拡張
+//[CustomEditor(typeof(SoundManager))]
 
-public class SoundManagerEditor : Editor
-{
-    void OnEnable()
-    {
+//public class SoundManagerEditor : Editor
+//{
+//    void OnEnable()
+//    {
 
-    }
-    public override void OnInspectorGUI()
-    {
-        SoundManager Sound = target as SoundManager;
+//    }
+//    public override void OnInspectorGUI()
+//    {
+//        SoundManager Sound = target as SoundManager;
 
-        // エディターの変更確認
-        EditorGUI.BeginChangeCheck();
+//        // エディターの変更確認
+//        EditorGUI.BeginChangeCheck();
 
-        //Sound.AudioClipList = EditorGUILayout
+//        //Sound.AudioClipList = EditorGUILayout
 
 
-        // エディターの変更確認
-        if (EditorGUI.EndChangeCheck())
-        {
-            EditorUtility.SetDirty(target); // 選択オブジェクト更新
-        }
-    }
-}
-#endif
+//        // エディターの変更確認
+//        if (EditorGUI.EndChangeCheck())
+//        {
+//            EditorUtility.SetDirty(target); // 選択オブジェクト更新
+//        }
+//    }
+//}
+//#endif
 
 
 public enum SOUND_TYPE
@@ -41,17 +41,25 @@ public enum SOUND_TYPE
 }
 
 // サウンドのクリップ管理用
-public class Audio_Clip
+public struct Audio_Clip
 {
     public string AudioName;    // クリップに付ける名前
     public AudioClip AudioClip; // オーディオクリップ
-    public SOUND_TYPE SoundType;
+    public SOUND_TYPE SoundType;// 音の種類
 }
 
 public class SoundManager : SingletonMonoBehaviour<SoundManager>
 {
-    public List<Audio_Clip> AudioClipList = new List<Audio_Clip>();
-    private List<AudioSource> AudioSourceList = new List<AudioSource>();
+    [SerializeField] private int AudioSource_BGM_Max = 3;   // オーディオソースBGMの最大数(インスペクターで表示)
+    [SerializeField] private int AudioSource_SE_Max = 10;   // オーディオソースSEの最大数(インスペクターで表示)
+
+    private static int AudioSource_BGM_MAX;   // オーディオソースBGMの最大数(static)
+    private static int AudioSource_SE_MAX;    // オーディオソースSEの最大数(static)
+
+
+    public List<Audio_Clip> AudioClipList = new List<Audio_Clip>(); // オーディオクリップのリスト
+    private AudioSource[] AudioSource_BGM;  // オーディオソースBGM
+    private AudioSource[] AudioSource_SE;   // オーディオソースSE
 
 
 
@@ -64,8 +72,16 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
         }
 
         DontDestroyOnLoad(this.gameObject); // シーンが変わっても死なない
+
+        // 初期化
+        AudioSource_BGM_MAX = AudioSource_BGM_Max;
+        AudioSource_SE_MAX = AudioSource_SE_Max;
+        AudioSource_BGM = new AudioSource[AudioSource_BGM_MAX];
+        AudioSource_SE = new AudioSource[AudioSource_SE_MAX];
     }
 
+
+    // 音の再生
     public void PlaySound(string AudioName, bool Loop)
     {
         AudioClip audioClip = null;
@@ -83,22 +99,19 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
         if (audioClip == null)
             return;
 
-        // オーディオソースの生成
-        AudioSourceList.Add(this.gameObject.AddComponent<AudioSource>());
-
         // 再生
-        AudioSourceList[AudioSourceList.Count - 1].PlayOneShot(audioClip, 1.0f);
+        //AudioSourceList.PlayOneShot(audioClip, 1.0f);
     }
 
     public void FixedUpdate()
     {
-        for (int i = 0; i < AudioSourceList.Count; i++)
-        {
-            if (AudioSourceList[i].isPlaying)
-            {
-                AudioSourceList.RemoveAt(i);
-                Debug.Log("再生終了");
-            }
-        }
+        //for (int i = 0; i < AudioSourceList.Count; i++)
+        //{
+        //    if (AudioSourceList[i].isPlaying)
+        //    {
+        //        AudioSourceList.RemoveAt(i);
+        //        Debug.Log("再生終了");
+        //    }
+        //}
     }
 }
