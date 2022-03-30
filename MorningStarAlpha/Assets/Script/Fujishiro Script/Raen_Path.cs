81eb88c3ddc7d79a6846d4983ebed447e9f592b7
藤代 <qqcu8ef9k@peace.ocn.ne.jp>
2022-03-30 17:07:20 +0900

    DoTweeen作った

--- MorningStarAlpha/Assets/Script/Fujishiro Script/Raen_Path.cs
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/Raen_Path.cs
@@ -2,28 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using DG.Tweening;
 
 public class Raen_Path : MonoBehaviour
 {
-    [Serializable] public struct Waypoint
-    {
-        [Tooltip("ウェイポイントの位置")]
-        public Vector3 position;
-
-        [Tooltip("ウェイポイントに到達したときの角度")]
-        public float roll;
-    }
-
-    public Waypoint[] m_waypoints = new Waypoint[0];
+    public Vector3[] Waypoint;
+    // public Raen_Path instance; // パス情報
 
     // アタッチしたときに最初に設定される
     void Reset()
     {
-        m_waypoints = new Waypoint[2]
-        {
-            new Waypoint{position = new Vector3(0, 0, 0), roll = 0},
-            new Waypoint{position = new Vector3(0, 0, 0),roll = 0}
-        };
+        
+    }
+
+    void Awake()
+    {
+        Path_cart.waypoint = this;
     }
 }
