e485dc1b0b1049ef573d3a84db04dcb09935b00c
TaKeToShi800 <uetaketoshiaki.123@gmail.com>
2022-03-31 11:42:12 +0900

    エフェクト親オブジェクトも消えるように

--- MorningStarAlpha/Assets/Script/Uetake Script/AutoDestroyEffect.cs
+++ MorningStarAlpha/Assets/Script/Uetake Script/AutoDestroyEffect.cs
@@ -9,15 +9,5 @@ public class AutoDestroyEffect : MonoBehaviour
 
     private Renderer renderer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
