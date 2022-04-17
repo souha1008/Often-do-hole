fc8271f6e450b637c50e9fa1311479df5cb0de33
藤代 <qqcu8ef9k@peace.ocn.ne.jp>
2022-04-17 16:03:10 +0900

    リザルト調整中

--- MorningStarAlpha/Assets/Script/Fujishiro Script/GoalManager.cs
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/GoalManager.cs
@@ -9,8 +9,8 @@ public class GoalManager : MonoBehaviour
 {
     [SerializeField] GameObject ClearCam;
     [SerializeField] GameObject MainCam;
-    [SerializeField] PostProcessProfile PostProssece;
-    [SerializeField] Image RawImage;
+    [SerializeField] GameObject PostProssece;
+    [SerializeField] RawImage RawImage;
 
     private int counter_1; // 調整カウント用
     private float alpha;
@@ -20,7 +20,7 @@ public class GoalManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // カメラを非アクティブ
+        // クリアカメラ非アクティブ
         ClearCam.SetActive(false);
 
         // 変数初期化
@@ -36,14 +36,14 @@ public class GoalManager : MonoBehaviour
     {
         if (AngleChange == true)
         {
-            MainCam.transform.Rotate(new Vector3(0.2f, 0, 0));
+            MainCam.transform.Rotate(new Vector3(-0.2f, 0, 0));
             counter_1++;
         }
 
-        if (counter_1 <= 4)
+        if (counter_1 >= 4)
         {
-            alpha++;
-            RawImage.color += new Color(1, 1, 1, alpha);
+            alpha += 0.1f;
+            RawImage.color += new Color(0, 0, 0, alpha);
         }
 
         // リザルト画面への遷移判定
@@ -58,7 +58,17 @@ public class GoalManager : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             AngleChange = true;
-            PostProssece.AddSettings<MotionBlur>().enabled.Override(true);
+            //PostProcessVolume ppv = PostProssece.GetComponent<PostProcessVolume>();
+            //if (ppv == null) Debug.Log("ppv is not loading");
+
+            //PostProcessProfile ppp = ppv.profile;
+            //if (ppp == null) Debug.Log("ppp is not loading");
+
+            //ppp.AddSettings<MotionBlur>().enabled.Override(true);
+
+            ClearCam.SetActive(true);
+            if (!ClearCam.activeSelf) Debug.Log("ClearCam is not Actived");
+            
         }
 
         Debug.Log("Goal当たった");
