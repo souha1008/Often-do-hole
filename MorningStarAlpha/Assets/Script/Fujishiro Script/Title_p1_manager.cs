063b47bb3c0936d2acfa52519a8164feef311649
藤代 <qqcu8ef9k@peace.ocn.ne.jp>
2022-04-27 20:41:56 +0900

    タイトル遷移カメラ移動

--- MorningStarAlpha/Assets/Script/Fujishiro Script/Title_p1_manager.cs
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/Title_p1_manager.cs
@@ -4,28 +4,47 @@ using UnityEngine;
 
 public class Title_p1_manager : MonoBehaviour
 {
-    [SerializeField] Camera camera;
+    [Tooltip("メインカメラ")][SerializeField] Camera camera;
+    [Tooltip("Playerオブジェクト")][SerializeField] GameObject Player;
 
-    private bool transition;
+    [Tooltip("カメラが動いているフレーム")][SerializeField] int camera_motion = 30;
+
+    private bool transition;    // カメラのモーショントリガー
+    Vector3 pos;                // プレイヤー位置補正用
+    int flame_count;
+
+    void Awake()
+    {
+        pos = Player.transform.position;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         transition = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("A") || Input.GetButton("B")
-            || Input.GetButton("X") || Input.GetButton("Y"))
+        // PlayerのZ軸補正
+        Player.transform.position = pos;
+
+        if(Input.GetButton("Fire1") || Input.GetButton("Fire2")
+            || Input.GetButton("Fire3") || Input.GetButton("Jump"))
         {
             transition = true;
         }
 
         if(transition == true)
         {
-            //camera.transform.Rotate(new Vector3())
+            flame_count++;
+            camera.transform.Rotate(new Vector3(0, 5.4f, 0));
+            if(flame_count >= camera_motion)
+            {
+                transition = false;
+            }
         }
     }
 }
