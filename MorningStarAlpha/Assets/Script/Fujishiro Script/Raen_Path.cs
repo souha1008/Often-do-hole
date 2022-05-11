04ac67bffe01e3efefe7d3aac883d6b69b8237fd
TaKeToShi800 <uetaketoshiaki.123@gmail.com>
2022-05-11 16:28:23 +0900

    プレファブ化

--- MorningStarAlpha/Assets/Script/Fujishiro Script/Raen_Path.cs
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/Raen_Path.cs
@@ -1,16 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using System.Linq;
 
 public class Raen_Path : MonoBehaviour
 {
     public GameObject[] positions; // ウェイポイント配列
     public GameObject[] player_postions;
 
-    void Awake()
+    private Vector3[] path;
+    private Vector3[] player_path;
+
+
+    private bool Rall_Start = false;
+    private bool Rall_Now = false;
+
+    void Start()
     {
-        Path_cart.waypoints = this;
+        Rall_Start = false;
+        Rall_Now = false;
     }
 
-    
+    void Update()
+    {
+      
+        if (Rall_Start == true && Rall_Now == false)
+        {
+            positions[0] = BulletMain.instance.gameObject;
+            player_postions[0] = PlayerMain.instance.gameObject;
+
+            path = positions.Select(target => target.transform.position).ToArray();
+            player_path = player_postions.Select(target => target.transform.position).ToArray();
+
+            BulletMain.instance.transform
+            .DOPath(path, 4.0f, PathType.Linear)
+            .OnUpdate(() =>
+            {
+
+            })
+            .OnComplete(() =>
+             {
+                 Debug.Log("Complete");
+                 PlayerState.PlayerScript.mode = new PlayerStateMidair(true, MidairState.NORMAL);
+             });
+
+            PlayerMain.instance.transform
+            .DOPath(player_path, 4.0f);
+
+            Rall_Now = true;                                       // 一回のみ用フラグをオン
+        }
+        
+}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Bullet") && Rall_Now == false)
+        {
+            Rall_Start = true;
+        }
+    }
+
 }
