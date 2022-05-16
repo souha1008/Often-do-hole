2b44f48ab50878c51342dceadcd7323ee4e57c9f
MSI\clclm <hal88k5@gmail.com>
2022-05-16 19:11:35 +0900

    ステージセレクト 素材入れるだけだよ

--- MorningStarAlpha/Assets/Script/ShimokawaraScript/StageSelect/DrawStageName.cs
+++ MorningStarAlpha/Assets/Script/ShimokawaraScript/StageSelect/DrawStageName.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawStageName : MonoBehaviour
 {
+    private string[] StageNames = {
+        "Stage1",
+        "Stage2",
+        "Stage3",
+        "Stage4",
+        "Stage5",
+        "Stage6",
+        "Stage7",
+        "Stage8",
+        "Stage9",
+        "Stage10",
+        "Stage11"
+    };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,7 @@ public class DrawStageName : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(SelectManager.instance.CanStart)
+        gameObject.GetComponent<UnityEngine.UI.Text>().text = StageNames[SelectManager.instance.NowSelectStage];
     }
 }
