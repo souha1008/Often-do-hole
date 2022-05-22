46856b4d158f7b305aa926be131788177d4f19b8
高平 大数 <takatai2020game@gmail.com>
2022-05-22 20:44:48 +0900

    鎖修正 棘,穴修正

--- MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_Hole.cs
+++ MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_Hole.cs
@@ -32,4 +32,17 @@ public class Gimmick_Hole : Gimmick_Main
             }
         }
     }
+
+    public void OnTriggerStay(Collider collider)
+    {
+        if (PlayerMain.instance.refState != EnumPlayerState.DEATH)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                // プレイヤーを死亡状態に変更
+                PlayerMain.instance.mode = new PlayerStateDeath_Void();
+                // プレイヤーにダメージエフェクト
+            }
+        }
+    }
 }
