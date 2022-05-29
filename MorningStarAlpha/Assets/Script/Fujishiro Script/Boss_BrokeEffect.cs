ef4ecf2346df67ce7b885d957f6d8bf0ba94c674
FujishiroHaruki_notePC <fujishiro.haruki.hal@gmail.com>
2022-05-29 15:48:30 +0900

    ボスが壁に当たった時エフェクトを発生させる

--- MorningStarAlpha/Assets/Script/Fujishiro Script/Boss_BrokeEffect.cs
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/Boss_BrokeEffect.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Boss_BrokeEffect : MonoBehaviour
 {
     [SerializeField] ParticleSystem brokeEffect;
+    [SerializeField] GameObject _brokeEffect;
+    [SerializeField] int loop_num = 3;
+    [SerializeField] float effect_interval = 30.0f;
+    [SerializeField] Vector3 scale_change = new Vector3(0.3f, 0.3f, 0.3f);
 
     Vector3 pos;    // エフェクト発生場所
     
@@ -12,6 +16,7 @@ public class Boss_BrokeEffect : MonoBehaviour
     void Start()
     {
         pos = this.gameObject.transform.position;
+        pos.y += 30;
     }
 
     // Update is called once per frame
@@ -22,17 +27,22 @@ public class Boss_BrokeEffect : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Boss"))
+        if(other.gameObject.tag == "Boss")
         {
             this.gameObject.SetActive(false);
             
-            for(int i = 0; i < 6; i++)
+            for(int i = 0; i < loop_num; i++)
             {
-                ParticleSystem newParticle = Instantiate(brokeEffect);
+                //ParticleSystem newParticle = Instantiate(brokeEffect);
+                //newParticle.gameObject.transform.localScale = scale_change;
+                //newParticle.gameObject.transform.position = new Vector3(pos.x, pos.y - effect_interval * i, 0);
 
-                newParticle.gameObject.transform.position = new Vector3(pos.x, pos.y - 10 * i, pos.z);
+                //newParticle.Play();
 
-                newParticle.Play();
+                GameObject particle = Instantiate(_brokeEffect);
+                particle.transform.localScale = scale_change;
+                particle.gameObject.transform.position = new Vector3(pos.x, pos.y - effect_interval * i, 0);
+                //Destroy(newParticle.gameObject, 5.0f);
             }
         }
     }
