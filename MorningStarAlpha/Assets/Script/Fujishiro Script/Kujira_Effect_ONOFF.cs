df1f52d5b225b8dab2e68c836ce4feb03f9f6db6
藤代 <qqcu8ef9k@peace.ocn.ne.jp>
2022-05-29 12:59:39 +0900

    クジラエフェクトONOFF模索中

---
+++ MorningStarAlpha/Assets/Script/Fujishiro Script/Kujira_Effect_ONOFF.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kujira_Effect_ONOFF : MonoBehaviour
+{
+    [SerializeField] GameObject fog_1;
+    [SerializeField] GameObject fog_2;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        fog_1.SetActive(false);
+        fog_2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Iron"))
+        {
+            fog_1.SetActive(true);
+            fog_2.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if(collider.CompareTag("Iron"))
+        {
+            fog_1.SetActive(false);
+            fog_2.SetActive(false);
+        }
+    }
+}
