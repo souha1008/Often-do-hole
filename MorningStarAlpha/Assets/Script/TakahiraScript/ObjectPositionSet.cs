b51564896b094499cb727026ec2ad8ee0f2dbb88
たいたい <takatai2020game@gmail.com>
2022-09-30 12:42:55 +0900

    3.2ずつ動くコンポーネント追加

---
+++ MorningStarAlpha/Assets/Script/TakahiraScript/ObjectPositionSet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+
+[SelectionBase]
+public class ObjectPositionSet : MonoBehaviour
+{
+    // GameConfig.GridSize を1目盛りとした場合の、グリッド座標
+    [SerializeField] public Vector2Int gridPos = Vector2Int.zero;
+    public static readonly float GridSize = 3.2f;
+
+    /// <summary>
+    /// グリッド移動量を指定して移動する
+    /// </summary>
+    public void Move(Vector2Int vec2)
+    {
+        gridPos += vec2;
+        transform.position = GetGlobalPosition(gridPos);
+    }
+
+    /// <summary>
+    /// グリッド座標をGlobal座標に変換する
+    /// </summary>
+    public static Vector3 GetGlobalPosition(Vector2Int gridPos)
+    {
+        return new Vector3(
+            gridPos.x * GridSize,
+            gridPos.y * GridSize,
+            0);
+    }
+}
