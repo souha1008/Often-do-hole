68485de7443708b0b5bf24c85eebe17927655c34
TAITAI-FRESHOSR\takat <takatai2020game@gmail.com>
2022-10-15 17:27:29 +0900

    ギミックキラー追加

--- MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_CannonChild.cs
+++ MorningStarAlpha/Assets/Script/TakahiraScript/Gimmick_CannonChild.cs
@@ -38,7 +38,7 @@ public class Gimmick_CannonChild : Gimmick_Main
         //Vel = CalculationScript.AngleVectorXY(CalculationScript.AngleCalculation(Rad.z)).normalized * Speed; // (ノーマライズ)
     }
 
-    public override void UpdateMove()
+    public override void FixedMove()
     {
         // 追尾処理
         if (ChaseFlag)
@@ -55,9 +55,10 @@ public class Gimmick_CannonChild : Gimmick_Main
 
         if (NowLifeTime >= LifeTime) // 生存時間で死亡
         {
+            //Debug.LogWarning("生存時間経過で死亡");
             Death();
         }
-        NowLifeTime += Time.deltaTime; // 時間加算
+        NowLifeTime += Time.fixedDeltaTime; // 時間加算
     }
 
     public override void Death()
@@ -65,46 +66,7 @@ public class Gimmick_CannonChild : Gimmick_Main
         //Debug.LogWarning("死亡");
         // 自分自身を消す
         //Destroy(this.gameObject);
+
         this.gameObject.SetActive(false);
-    }
-
-    public override void OnTriggerEnter(Collider collider)
-    {
-        //Debug.LogWarning(collider.gameObject.name);
-        // 弾だけ消える
-        if (collider.gameObject.CompareTag("Bullet") ||
-            (collider.gameObject.CompareTag("Player") && PlayerMain.instance.refState == EnumPlayerState.SWING))
-        {
-            // ヒットストップ
-            GameSpeedManager.Instance.StartHitStop(0.1f);
-
-            // 振動
-            VibrationManager.Instance.StartVibration(0.4f, 0.4f, 0.2f);
-
-            // 自身が死亡
-            Death();
-        }
-
-        // ノックバック
-        else if (collider.gameObject.CompareTag("Player"))
-        {
-            // ヒットストップ
-            //GameSpeedManager.Instance.StartHitStop(0.1f);
-
-            // エフェクト
-            EffectManager.Instance.SharkExplosionEffect(this.transform.position);
-
-            // 振動
-            VibrationManager.Instance.StartVibration(0.7f, 0.7f, 0.2f);
-
-            // プレイヤーをノックバック状態に変更
-            PlayerMain.instance.mode = new PlayerState_Knockback(this.transform.position, false);
-
-            // 自身が死亡
-            Death();
-        }
-
-        // 死亡
-        Death();
     }
 }
